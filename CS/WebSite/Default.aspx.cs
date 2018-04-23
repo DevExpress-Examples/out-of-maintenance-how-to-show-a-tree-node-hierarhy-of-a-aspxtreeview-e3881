cf60b0e4a4f17bd90319e04b3377242b4dce685e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+using DevExpress.Web.ASPxClasses;
+using DevExpress.Web.ASPxTreeView;
+using DevExpress.Web.ASPxEditors;
+
+public partial class _Default : System.Web.UI.Page {
+    const string FileImageUrl = "~/Images/file.png";
+    const string DirImageUrl = "~/Images/directory.png";
+
+    protected void treeView_VirtualModeCreateChildren(object source, TreeViewVirtualModeCreateChildrenEventArgs e) {
+        string parentNodePath = string.IsNullOrEmpty(e.NodeName) ? Page.MapPath("~/") : e.NodeName;
+        List<TreeViewVirtualNode> children = new List<TreeViewVirtualNode>();
+        if(Directory.Exists(parentNodePath)) {
+            foreach(string childPath in Directory.GetDirectories(parentNodePath)) {
+                string childDirName = Path.GetFileName(childPath);
+                if(IsSystemName(childDirName))
+                    continue;
+                TreeViewVirtualNode childNode = new TreeViewVirtualNode(childPath, childDirName);
+                childNode.Image.Url = DirImageUrl;
+                children.Add(childNode);
+            }
+            foreach(string childPath in Directory.GetFiles(parentNodePath)) {
+                string childFileName = Path.GetFileName(childPath);
+                if(IsSystemName(childFileName))
+                    continue;
+                TreeViewVirtualNode childNode = new TreeViewVirtualNode(childPath, childFileName);
+                childNode.IsLeaf = true;
+                childNode.Image.Url = FileImageUrl;
+                children.Add(childNode);
+            }
+        }
+        e.Children = children;
+    }
+
+    bool IsSystemName(string name) {
+        name = name.ToLower();
+        return name.StartsWith("app_") || name == "bin" || name.EndsWith(".aspx.cs") || name.EndsWith(".aspx.vb");
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,53 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.IO
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Collections.Generic
+Imports DevExpress.Web.ASPxClasses
+Imports DevExpress.Web.ASPxTreeView
+Imports DevExpress.Web.ASPxEditors
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private Const FileImageUrl As String = "~/Images/file.png"
+	Private Const DirImageUrl As String = "~/Images/directory.png"
+
+	Protected Sub treeView_VirtualModeCreateChildren(ByVal source As Object, ByVal e As TreeViewVirtualModeCreateChildrenEventArgs)
+		Dim parentNodePath As String
+		If String.IsNullOrEmpty(e.NodeName) Then
+			parentNodePath = Page.MapPath("~/")
+		Else
+			parentNodePath = e.NodeName
+		End If
+		Dim children As New List(Of TreeViewVirtualNode)()
+		If Directory.Exists(parentNodePath) Then
+			For Each childPath As String In Directory.GetDirectories(parentNodePath)
+				Dim childDirName As String = Path.GetFileName(childPath)
+				If IsSystemName(childDirName) Then
+					Continue For
+				End If
+				Dim childNode As New TreeViewVirtualNode(childPath, childDirName)
+				childNode.Image.Url = DirImageUrl
+				children.Add(childNode)
+			Next childPath
+			For Each childPath As String In Directory.GetFiles(parentNodePath)
+				Dim childFileName As String = Path.GetFileName(childPath)
+				If IsSystemName(childFileName) Then
+					Continue For
+				End If
+				Dim childNode As New TreeViewVirtualNode(childPath, childFileName)
+				childNode.IsLeaf = True
+				childNode.Image.Url = FileImageUrl
+				children.Add(childNode)
+			Next childPath
+		End If
+		e.Children = children
+	End Sub
+
+	Private Function IsSystemName(ByVal name As String) As Boolean
+		name = name.ToLower()
+		Return name.StartsWith("app_") OrElse name = "bin" OrElse name.EndsWith(".aspx.cs") OrElse name.EndsWith(".aspx.vb")
+	End Function
+End Class
